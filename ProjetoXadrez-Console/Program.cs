9aa83cad1224df3b34bb8aaf8a6acf0fd882167b
Lucas Guedes <lcsguedesf@gmail.com>
2024-02-20 18:52:33 -0300

    tratando erros com exceção personalizada

--- ProjetoXadrez-Console/Program.cs
+++ ProjetoXadrez-Console/Program.cs
@@ -8,15 +8,20 @@ namespace projetoXadrez_Console
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
-            Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+                tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(0, 0));
+                tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(1, 3));
+                tabuleiro.ColocarPeca(new Rei(Cor.Preta, tabuleiro), new Posicao(0, 9));
 
-            tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(0, 0));
-            tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(1, 3));
-            tabuleiro.ColocarPeca(new Rei(Cor.Preta, tabuleiro), new Posicao(2, 4));
-
-            Tela.ImprimirTabuleiro(tabuleiro);
-
+                Tela.ImprimirTabuleiro(tabuleiro);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
     }
